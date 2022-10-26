0afba5b8eff885d70dc8b68f8b6ea9efce818039
_ <simon.roberts@kaycare.co.uk>
2022-10-26 15:53:41 +0100

    Update utils.py

--- main/utils.py
+++ main/utils.py
@@ -13,29 +13,29 @@ from pytorch_lightning import Callback
 from pytorch_lightning.utilities import rank_zero_only
 
 
-def get_logger(name=__name__) -> logging.Logger:
-    """Initializes multi-GPU-friendly python command line logger."""
-
-    logger = logging.getLogger(name)
-
-    # this ensures all logging levels get marked with the rank zero decorator
-    # otherwise logs would get multiplied for each GPU process in multi-GPU setup
-    for level in (
-        "debug",
-        "info",
-        "warning",
-        "error",
-        "exception",
-        "fatal",
-        "critical",
-    ):
-        setattr(logger, level, rank_zero_only(getattr(logger, level)))
-
-    return logger
-
-
-log = get_logger(__name__)
-
+#def get_logger(name=__name__) -> logging.Logger:
+#    """Initializes multi-GPU-friendly python command line logger."""#
+#
+#    logger = logging.getLogger(name)
+#
+#    # this ensures all logging levels get marked with the rank zero decorator
+#    # otherwise logs would get multiplied for each GPU process in multi-GPU setup
+#    for level in (
+#        "debug",
+#        "info",
+#        "warning",
+#        "error",
+#        "exception",
+#        "fatal",
+#        "critical",
+#    ):
+#        setattr(logger, level, rank_zero_only(getattr(logger, level)))#
+#
+#    return logger
+##
+#
+#log = get_logger(__name__)
+#
 
 def extras(config: DictConfig) -> None:
     """Applies optional utilities, controlled by config flags.
@@ -105,49 +105,49 @@ def print_config(
         rich.print(tree, file=file)
 
 
-@rank_zero_only
-def log_hyperparameters(
-    config: DictConfig,
-    model: pl.LightningModule,
-    datamodule: pl.LightningDataModule,
-    trainer: pl.Trainer,
-    callbacks: List[pl.Callback],
-    logger: List[pl.loggers.LightningLoggerBase],
-) -> None:
-    """Controls which config parts are saved by Lightning loggers.
-    Additionaly saves:
-    - number of model parameters
-    """
-
-    if not trainer.logger:
-        return
-
-    hparams = {}
-
-    # choose which parts of hydra config will be saved to loggers
-    hparams["model"] = config["model"]
-
-    # save number of model parameters
-    hparams["model/params/total"] = sum(p.numel() for p in model.parameters())
-    hparams["model/params/trainable"] = sum(
-        p.numel() for p in model.parameters() if p.requires_grad
-    )
-    hparams["model/params/non_trainable"] = sum(
-        p.numel() for p in model.parameters() if not p.requires_grad
-    )
-
-    hparams["datamodule"] = config["datamodule"]
-    hparams["trainer"] = config["trainer"]
-
-    if "seed" in config:
-        hparams["seed"] = config["seed"]
-    if "callbacks" in config:
-        hparams["callbacks"] = config["callbacks"]
-
-    hparams["pacakges"] = get_packages_list()
-
-    # send hparams to all loggers
-    trainer.logger.log_hyperparams(hparams)
+#@rank_zero_only
+#def log_hyperparameters(
+#    config: DictConfig,
+#    model: pl.LightningModule,
+#    datamodule: pl.LightningDataModule,
+#    trainer: pl.Trainer,
+#    callbacks: List[pl.Callback],
+#    logger: List[pl.loggers.LightningLoggerBase],
+#) -> None:
+#    """Controls which config parts are saved by Lightning loggers.
+#    Additionaly saves:
+#    - number of model parameters
+#    """
+#
+#    if not trainer.logger:
+#        return
+#
+#    hparams = {}
+#
+#    # choose which parts of hydra config will be saved to loggers
+#    hparams["model"] = config["model"]
+#
+#    # save number of model parameters
+#    hparams["model/params/total"] = sum(p.numel() for p in model.parameters())
+#    hparams["model/params/trainable"] = sum(
+#        p.numel() for p in model.parameters() if p.requires_grad
+#    )
+#    hparams["model/params/non_trainable"] = sum(
+#        p.numel() for p in model.parameters() if not p.requires_grad
+#    )
+#
+#    hparams["datamodule"] = config["datamodule"]
+#    hparams["trainer"] = config["trainer"]
+#
+#    if "seed" in config:
+#        hparams["seed"] = config["seed"]
+#    if "callbacks" in config:
+#        hparams["callbacks"] = config["callbacks"]
+#
+#    hparams["pacakges"] = get_packages_list()
+#
+#    # send hparams to all loggers
+#    trainer.logger.log_hyperparams(hparams)
 
 
 def finish(
@@ -156,16 +156,16 @@ def finish(
     datamodule: pl.LightningDataModule,
     trainer: pl.Trainer,
     callbacks: List[pl.Callback],
-    logger: List[pl.loggers.LightningLoggerBase],
+   #logger: List[pl.loggers.LightningLoggerBase],
 ) -> None:
     """Makes sure everything closed properly."""
 
     # without this sweeps with wandb logger might crash!
-    for lg in logger:
-        if isinstance(lg, pl.loggers.wandb.WandbLogger):
-            import wandb
+    #for lg in logger:
+    #    if isinstance(lg, pl.loggers.wandb.WandbLogger):
+    #        import wandb
 
-            wandb.finish()
+    #        wandb.finish()
 
 
 def get_packages_list() -> List[str]:
@@ -199,6 +199,6 @@ class SavePytorchModelAndStopCallback(Callback):
         os.makedirs(os.path.split(path)[0], exist_ok=True)
         # Save model
         torch.save(model, path)
-        log.info(f"PyTorch model saved at: {path}")
+        #log.info(f"PyTorch model saved at: {path}")
         # Stop trainer
         trainer.should_stop = True
