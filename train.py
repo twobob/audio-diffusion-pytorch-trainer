48a87722c65a37e6ef6204f6d5e66d18823ad01d
_ <simon.roberts@kaycare.co.uk>
2022-10-29 06:18:29 +0100

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from omegaconf import DictConfig, open_dict
 
 # Load environment variables from `.env`.
 dotenv.load_dotenv(override=True)
-log = utils.get_logger(__name__)
+#log = utils.get_logger(__name__)
 
 
 @hydra.main(config_path=".", config_name="config.yaml", version_base=None)
@@ -49,45 +49,46 @@ def main(config: DictConfig) -> None:
                 callbacks.append(hydra.utils.instantiate(cb_conf, _convert_="partial"))
 
     # Initialize loggers (e.g. wandb)
-    loggers = []
-    if "loggers" in config:
-        for _, lg_conf in config["loggers"].items():
-            if "_target_" in lg_conf:
-                log.info(f"Instantiating logger <{lg_conf._target_}>.")
-                # Sometimes wandb throws error if slow connection...
-                logger = utils.retry_if_error(
-                    lambda: hydra.utils.instantiate(lg_conf, _convert_="partial")
-                )
-                loggers.append(logger)#
+    #loggers = []
+    #if "loggers" in config:
+    #    for _, lg_conf in config["loggers"].items():
+    #        if "_target_" in lg_conf:
+    #            log.info(f"Instantiating logger <{lg_conf._target_}>.")
+    #            # Sometimes wandb throws error if slow connection...
+    #            logger = utils.retry_if_error(
+    #                lambda: hydra.utils.instantiate(lg_conf, _convert_="partial")
+    #            )
+    #            loggers.append(logger)#
 
     # Initialize trainer
-    log.info(f"Instantiating trainer <{config.trainer._target_}>.")
+    #log.info(f"Instantiating trainer <{config.trainer._target_}>.")
     trainer = hydra.utils.instantiate(
-        config.trainer, callbacks=callbacks, logger=loggers, _convert_="partial"
+        #config.trainer, callbacks=callbacks, logger=loggers, _convert_="partial"
+        config.trainer, callbacks=callbacks, _convert_="partial"
     )
 
     # Send some parameters from config to all lightning loggers
-    log.info("Logging hyperparameters!")
-    utils.log_hyperparameters(
-        config=config,
-        model=model,
-        datamodule=datamodule,
-        trainer=trainer,
-        callbacks=callbacks#,
+    #log.info("Logging hyperparameters!")
+    #utils.log_hyperparameters(
+    #    config=config,
+    #    model=model,
+    #    datamodule=datamodule,
+    #    trainer=trainer,
+    #    callbacks=callbacks,
     #    logger=loggers,
-    )
+    #)
 
     # Train with checkpoint if present, otherwise from start
     if "ckpt" in config:
         ckpt = config.get("ckpt")
-        log.info(f"Starting training from {ckpt}")
+        #log.info(f"Starting training from {ckpt}")
         trainer.fit(model=model, datamodule=datamodule, ckpt_path=ckpt)
     else:
-        log.info("Starting training.")
+        #log.info("Starting training.")
         trainer.fit(model=model, datamodule=datamodule)
 
     # Make sure everything closed properly
-    log.info("Finalizing!")
+    #log.info("Finalizing!")
     utils.finish(
         config=config,
         model=model,
@@ -103,8 +104,10 @@ def main(config: DictConfig) -> None:
         and config.get("train")
         and not config.get("save")
     ):
-        log.info(f"Best model ckpt at {trainer.checkpoint_callback.best_model_path}")
+        print(f"Best model ckpt at {trainer.checkpoint_callback.best_model_path}")
 
 
+        
+        
 if __name__ == "__main__":
     main()
