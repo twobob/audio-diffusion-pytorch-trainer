fd63bcabd9c9f1cee3f6a46081e176bbe355db1c
_ <simonjohnroberts@msn.com>
2022-11-06 16:41:18 +0000

    feat: debug lengths

--- main/module_base.py
+++ main/module_base.py
@@ -98,6 +98,7 @@ class Datamodule(pl.LightningDataModule):
         print('self.data_train length',len(self.data_train),'self.data_val length', len(self.data_val))
 
     def train_dataloader(self) -> DataLoader:
+        print('self.data_train length', len(self.data_train))
         return DataLoader(
             dataset=self.data_train,
             batch_size=self.batch_size,
@@ -107,6 +108,7 @@ class Datamodule(pl.LightningDataModule):
         )
 
     def val_dataloader(self) -> DataLoader:
+        print('self.data_val length', len(self.data_val))
         return DataLoader(
             dataset=self.data_val,
             batch_size=self.batch_size,
